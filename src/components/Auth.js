8c97d486e07045c3a7708f98fd36c099dc4b7bfa
gc0rtes <guilhermeco@gmail.com>
2021-09-07 14:16:15 +0200

    Connect client-server

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -4,6 +4,8 @@ import axios from "axios";
 
 import signinImage from "../assets/signup.jpg";
 
+const cookies = new Cookies();
+
 const initialState = {
   fullName: "",
   username: "",
@@ -24,8 +26,39 @@ const Auth = () => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const { username, password, phoneNumber, avatarURL } = form;
+
+    const URL = "http://localhost:5000/auth";
+    // const URL = 'https://medical-pager.herokuapp.com/auth';
+
+    // Sending request to two different url depending if is signin or signup
+    const {
+      data: { token, userId, hashedPassword, fullName },
+    } = await axios.post(`${URL}/${isSignup ? "signup" : "login"}`, {
+      username,
+      password,
+      fullName: form.fullName,
+      phoneNumber,
+      avatarURL,
+    });
+
+    // Storing all the user data into cookies so we can hav it across all application
+    cookies.set("token", token);
+    cookies.set("username", username);
+    cookies.set("fullName", fullName);
+    cookies.set("userId", userId);
+
+    if (isSignup) {
+      cookies.set("phoneNumber", phoneNumber);
+      cookies.set("avatarURL", avatarURL);
+      cookies.set("hashedPassword", hashedPassword);
+    }
+
+    // Once we set the cookies we can reset our browser
+    window.location.reload();
+
     // console.log(form);
   };
 
